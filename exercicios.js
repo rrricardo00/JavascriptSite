e28b18463234ce11914ddf129e7bc5a73ee28430
rrricardo00 <rrricardo00@yahoo.com.br>
2019-12-03 22:45:06 -0300

    widths and heights

--- exercicios.js
+++ exercicios.js
@@ -35,3 +35,69 @@ imgs.forEach(function(element, index, array) {
 paragrafos.forEach(element => {
     console.log(element.textContent);
 }); */
+
+/* const menu = document.querySelector('.menu'); */
+
+/* menu.classList; */
+/* 
+const h2 = document.querySelector('h2');
+const pegar = h2.getBoundingClientRect();
+
+if (pegar.top < 0) {
+    console.log('Passou do scroll');
+
+}
+
+console.log(
+
+    window.innerWidth,
+    window.outerWidth,
+    window.innerHeight,
+    window.outerHeight,
+    window.pageYOffset,
+    window.pageXOffset
+
+); */
+
+/* const tamanho = window.matchMedia('(max-width:600px)').matches;
+
+if (tamanho) {
+    console.log('Usuário mobile')
+} else {
+    console.log('Usuário table>desktop')
+} */
+
+/* function somarImagens() {
+    const imgs = document.querySelectorAll('img');
+    let soma = 0;
+    imgs.forEach(element => {
+        soma += element.offsetWidth;
+    });
+    console.log(soma);
+}
+
+
+window.onload = () => {
+    somarImagens();
+} */
+
+/* const links = document.querySelectorAll('a');
+
+links.forEach(element => {
+    const lado = element.offsetWidth;
+    const altura = element.offsetHeight;
+
+
+    if (lado >= 48 && altura >= 48) {
+        console.log('Todos os links estão no padrão google');
+    } else {
+        console.log("Nem todos os links estão padronizados");
+    }
+
+}); */
+
+const small = window.matchMedia('(max-width:720px)').matches;
+if (small) {
+    const menu = document.querySelector('.menu');
+    menu.classList.add('.menu-mobile');
+}
