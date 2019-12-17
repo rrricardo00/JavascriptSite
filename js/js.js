faacdb93b029b9084bfa760b8a9a7fd4e2a94c99
rrricardo00 <rrricardo00@yahoo.com.br>
2019-12-17 01:20:54 -0300

    alpha

--- js/js.js
+++ js/js.js
@@ -44,6 +44,34 @@ function initAccordion() {
 
 }
 
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+
+        section.scrollIntoView({
+
+            behavior: 'smooth',
+            block: 'start',
+
+        });
+
+        // const topo = section.offsetTop;
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior: "smooth"
+        // });
+    }
+
+
+    linksInternos.forEach((element) => {
+        element.addEventListener('click', scrollToSection);
+    });
+}
 
 initTabNav();
 initAccordion();
+initScrollSuave();
