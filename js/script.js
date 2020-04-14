7eab19c55862395bbece020fa1c3ac93e0b6ba78
rrricardo00 <rrricardo00@yahoo.com.br>
2020-04-14 01:10:51 -0300

    aula

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ import initScrollSuave from './modules/scrollSuave.js';
 import scrollAnimado from './modules/scrollAnimado.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/Tab.js';
+import modal from './modules/modal.js';
+
 
 initTabNav();
 initScrollSuave();
 scrollAnimado();
 initAccordion();
+modal();
