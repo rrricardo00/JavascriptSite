45e1b2fcef1436540ec071525db3dcebb135a746
rrricardo00 <rrricardo00@yahoo.com.br>
2020-04-28 02:21:50 -0300

    aula

--- Button Creator/js.js
+++ Button Creator/js.js
@@ -41,11 +41,28 @@ function handleChange(event) {
     const name = event.target.name;
     const value = event.target.value;
     handleStyle[name](value);
+    saveValues(name, value);
     showCss();
 }
 
-function showCss() {
-    cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
+
+function saveValues(name, value) {
+    localStorage[name] = value;
+}
+
+function setValues() {
+    const properties = Object.keys(localStorage);
+    properties.forEach(propertie => {
+        handleStyle[propertie](localStorage[propertie]);
+        controles.elements[propertie].value = localStorage[propertie];
+    });
 
 }
+
+setValues();
+
+function showCss() {
+    cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
+}
+
 
