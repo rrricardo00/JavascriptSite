9051a8b9a3ae2af514dbb51baa9e32bbc362da8f
expert99 <rrricardo00@yahoo.com.br>
2020-08-04 18:32:41 -0300

    aula

--- FETCH/fetch-exercicios.js
+++ FETCH/fetch-exercicios.js
@@ -1,24 +1,68 @@
 
 const inputCep = document.querySelector('#cep');
-const btnCep = document.querySelector ('#btn');
-const resultadoCep = document.querySelector ('.resultadoCep');
+const btnCep = document.querySelector('#btn');
+const resultadoCep = document.querySelector('.resultadoCep');
 
-btnCep.addEventListener('click', handleClick);
+inputCep.addEventListener('keyup', handleClick);
 
 
-function handleClick(event){
+function handleClick(event) {
   event.preventDefault();
   const cep = inputCep.value;
   buscaCep(cep);
 }
 
-function buscaCep(cep){
+function buscaCep(cep) {
   fetch(`https://viacep.com.br/ws/${cep}/json/`)
-  .then(response =>{
-    return response.json();
-  }).then(response =>{
-    resultadoCep.innerText = response.localidade;
-    
-  })
+    .then(response => {
+      return response.text();
+    }).then(response => {
+      resultadoCep.innerText = response;
+
+    })
 }
 
+//BT2 ------------------------------------------------------------------------------------
+
+
+const btc = document.querySelector(".btc");
+
+function fetchBtc() {
+  fetch('https://blockchain.info/ticker')
+    .then(response => {
+      return response.json();
+    }).then(response => btc.innerText = ('R$ ' + response.BRL.buy).replace('.', ',')
+    )
+
+}
+
+fetchBtc();
+// setInterval(()=>fetchBtc(), 30000);
+
+function piada() {
+  fetch('https://api.chucknorris.io/jokes/random').then(response => response.json()).then(response => console.log(response.value))
+}
+
+
+piada();
+
+
+function corona() {
+
+
+  fetch('https://covid19-brazil-api.now.sh/api/report/v1')
+  .then(response => response.json())
+  .then(response => {
+    const numero = response.data.length;
+    for (let i = 0; i <= numero; i++) {
+      console.log(response.data[i].state + ' tem ' + response.data[i].cases + ' casos');
+      
+    }
+    
+  }
+
+  );
+
+}
+
+corona();
