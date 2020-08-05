ea8400ee3f88ece52cbee2b5af572be4c2a705b8
expert99 <rrricardo00@yahoo.com.br>
2020-08-04 23:44:24 -0300

    aula

--- FETCH/fetch-exercicios.js
+++ FETCH/fetch-exercicios.js
@@ -40,28 +40,42 @@ fetchBtc();
 // setInterval(()=>fetchBtc(), 30000);
 
 function piada() {
-  fetch('https://api.chucknorris.io/jokes/random').then(response => response.json()).then(response => console.log(response.value))
+  // fetch('https://api.chucknorris.io/jokes/random').then(response => response.json()).then(response => console.log(response.value))
 }
 
 
 piada();
 
+// CORONA---------------------------------------------------------------------------------------
+
+const cors = document.querySelector('.corona');
+
 
 function corona() {
 
-
   fetch('https://covid19-brazil-api.now.sh/api/report/v1')
-  .then(response => response.json())
-  .then(response => {
-    const numero = response.data.length;
-    for (let i = 0; i <= numero; i++) {
-      console.log(response.data[i].state + ' tem ' + response.data[i].cases + ' casos');
-      
-    }
-    
-  }
+    .then(response => response.json())
+    .then(response => {
+      const numero = response.data.length;
+      for (let i = 0; i <= numero; i++) {
+        const novo = document.createElement('p');
+        novo.innerText = response.data[i].state + ' tem ' + response.data[i].cases;
+        novo.classList.add('paragrafo-cors');
+        novo.style.flex = "1";
+        novo.style.margin = "20px";
+        cors.appendChild(novo);
+        const stringNova = novo.innerText.split('tem').join('<br/>');
+        novo.innerHTML = stringNova;
 
-  );
+
+
+      }
+
+
+    }
+
+    );
+
 
 }
 
