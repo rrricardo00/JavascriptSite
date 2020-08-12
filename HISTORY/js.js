2619340cbb67d45ea29fb65cab6b2740b98ef16b
expert99 <rrricardo00@yahoo.com.br>
2020-08-11 23:20:39 -0300

    aula

--- HISTORY/js.js
+++ HISTORY/js.js
@@ -7,17 +7,17 @@
 
 const links = document.querySelectorAll('a');
 
-links.forEach( link =>{
+links.forEach(link => {
     link.addEventListener('click', handleClick);
 });
 
-async function fetchPage(url){
+async function fetchPage(url) {
     const pageResponse = await fetch(url);
     const pagetext = await pageResponse.text();
     replaceContent(pagetext);
 }
 
-function replaceContent(newText){
+function replaceContent(newText) {
     const newHtml = document.createElement('div');
     newHtml.innerHTML = newText;
     const oldContent = document.querySelector('.content');
@@ -26,8 +26,37 @@ function replaceContent(newText){
     document.title = newHtml.querySelector('title').innerText;
 }
 
-function handleClick(event){
+function handleClick(event) {
     event.preventDefault();
     fetchPage(event.target.href);
+    window.history.pushState(null, null, event.target.href)
+}
+
+window.addEventListener('popstate', () => {
+
+    fetchPage(window.location.pathname)
+})
+
+
+const procurarInput = document.querySelector('.procurarInput');
+const botao = document.querySelector('.botao');
+const mostrarPokemon = document.querySelector('.mostrarPokemon');
+
+botao.addEventListener('click', pegarPokemon);
+
+
+function pegarPokemon(event) {
+    event.preventDefault();
+    const pegar = procurarInput.value;
+    pokemon(pegar)
+}
+
+async function pokemon(index) {
+    const pegar = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const mostrar = await pegar.json();
+    mostrarPokemon.innerText = mostrar.results[+index-1].name;
 
 }
+
+// pokemon()
+//bulbasaur
