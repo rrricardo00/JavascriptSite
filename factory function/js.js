c875a08fdddd84444ac1a0be731e66fd67a43390
expert99 <rrricardo00@yahoo.com.br>
2020-08-21 02:21:21 -0300

    aula

--- factory function/js.js
+++ factory function/js.js
@@ -32,3 +32,54 @@ Pessoa.prototype.andar = function () {
 const designer = /**new**/ Pessoa('Ricardo');
 
 
+//EXERCICIO------------------------------------
+function $$(selectedElements){
+ 
+    const elements = document.querySelectorAll(selectedElements);
+
+    function hide(){
+        elements.forEach(element =>{
+            element.style.display = "none";
+        });
+        return this; //se nao retornao o proprio objeto, retornar undefine e nao poderá encadear outro objeto
+    }
+    function show(){
+        elements.forEach(element =>{
+            element.style.display = "initial";
+        });
+        return this; //se nao retornao o proprio objeto, retornar undefine e nao poderá encadear outro objeto
+    }
+
+    function on(onEvent, callback){
+        elements.forEach(item =>{
+            item.addEventListener(onEvent, callback);
+        })
+        return this;
+    }
+    function addClass(className){
+        elements.forEach(item =>{
+            item.classList.add(className);
+        })
+        return this;
+    }
+    function removeClass(className){
+        elements.forEach(item =>{
+            item.classList.remove(className);
+        })
+        return this;
+    }
+
+    return {
+        elements,
+        hide,
+        show,
+        on,
+        addClass,
+        removeClass
+
+    }
+
+} 
+
+const btns = $$('button');
+
