3ed9f1c563a5b800aa927a40687f0a4dad090825
expert99 <rrricardo00@yahoo.com.br>
2020-09-07 04:18:28 -0300

    aula

--- destructuring/js.js
+++ destructuring/js.js
@@ -5,13 +5,13 @@ const carro = {
 
 const { marca, ano } = carro;
 
-console.log(marca, ano)
+// console.log(marca, ano)
 
 
 //-------------------------------------
 
 const cliente = {
-  nome: 'Andre',
+  nome: 'Ricardo',
   compras: {
     digitais: {
       livros: ['Livro 1', 'Livro 2'],
@@ -27,6 +27,9 @@ const cliente = {
 
 // console.log(livros);
 // console.log(videos);
+
+// const {digitais, digitais:{livros, videos}} = cliente.compras;
+// console.log(livros);
 
 
 //NESTING-------------------------------------
@@ -44,12 +47,12 @@ const cliente2 = {
   }
 }
 
-const { fisicas, digitais, digitais: { livros, videos } } = cliente2.compras;
+// const { fisicas, digitais, digitais: { livros, videos } } = cliente2.compras;
 
-console.log(fisicas);
-console.log(livros);
-console.log(videos);
-console.log(digitais);
+// console.log(fisicas);
+// console.log(livros);
+// console.log(videos);
+// console.log(digitais);
 
 
 //PROPRIEDADES
@@ -61,7 +64,7 @@ const cliente3 = {
 
 
 const { nome: nomeRicardo, email = "email@gmail.com" } = cliente3; //mudar variavel nome para nomeRicardo
-console.log(nomeRicardo, email)
+// console.log(nomeRicardo, email)
 
 //ARRAYS----------------------------------------------
 const frutas = ['Banana', 'Uva', 'Morango'];
@@ -72,3 +75,18 @@ const terceiraFruta = frutas[2];
 
 // Com destructuring
 const [primeira, segunda, terceira] = frutas;
+
+
+//EVENTO-----------------------------------------------------
+
+// function handleKeyboard(event) {
+//   console.log(event);
+// }
+
+// COM DESTRUCTURING
+function handleKeyboard({key, keyCode}) {
+  console.log(key, keyCode);
+}
+
+
+document.addEventListener('keyup', handleKeyboard);
